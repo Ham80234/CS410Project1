d1ebfd0f2f5dbe7be1b441b7a3c0bae0c5d20020
Andrew Hamlett <Ham80234@gmail.com>
2019-01-16 21:18:33 -0600

    Update Main.java
    Made the Boiler plate code fro making this massively scalable

--- Project/src/410Project1/src/Main.java
+++ Project/src/410Project1/src/Main.java
@@ -1,32 +1,55 @@
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
     private static String Delimeter = ",";
-    private static String[] temp_array;
+    private static ArrayList<String> temp_array = new ArrayList<String>();
 
-    public static void main(String[] args) throws IOException {
+
+
+
+    public static void main(String[] args) throws IOException{
+     ReadData("/Users/ud2131te/Documents/410Project1/src/File.dat");
+    }
+
+    private static void ReadData(String Path) throws IOException{
         DataInputStream data = new DataInputStream(new FileInputStream(
-                "/Users/ud2131te/Documents/410Project1/src/File.dat"));
+                Path));
+        int Counter = 0;
+        while (data.available() > 0) {
+            String line = data.readLine();
+            temp_array.add(line);
+            Counter++;
+        }
+        
 
-              while (data.available() > 0) {
-                    String line = data.readLine();
-                    temp_array = line.split(Delimeter);
-                    BreakArray(temp_array);
-            }
 
         data.close();
     }
 
-        public  static void BreakArray(String[] a){
-          String[] Titles = {"Address: ","Street: ","City: ","Zip: "};
-  
-          for (int i = 0; i < a.length; i++) {
-              System.out.println(Titles[i] + a[i]);
-          }
+    private static void BreakArray(String[] a){
+        String[] Titles = {"Address: ","Street: ","City: ","Zip: "};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(Titles[i] + a[i]);
+        }
 
     }
 
+    public static ArrayList<String> FindRoute(String Start, String End){
+        ArrayList<String> Route = new ArrayList<String>();
+
+
+
+        return Route;
+    }
+
+    public static void print(ArrayList<String> route){
+        for (int i = 0; i < route.size(); i++) {
+            System.out.println(route.get(i));
+        }
+    }
 }
